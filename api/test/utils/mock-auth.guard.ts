bd8834fae791c0d85a608c28791ef3113605dfc0
NarekEVN <narekvoskanyan3@gmail.com>
2025-09-23 17:49:27 +0400

    FIX: tests

--- api/test/utils/mock-auth.guard.ts
+++ api/test/utils/mock-auth.guard.ts
@@ -1,7 +1,12 @@
-import { CanActivate } from '@nestjs/common'
+import { CanActivate, ExecutionContext } from '@nestjs/common'
 
 export class MockAuthGuard implements CanActivate {
-  canActivate(): boolean {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest()
+    request.user = {
+      id: '7f21c221-363c-41b0-9b11-319d9c12dc34',
+      email: 'test@example.com',
+    }
     return true
   }
 }
